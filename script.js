55f069ac0d88dd93967d1972254992231ed0242c
Mohit <mohitshaharwale01@gmail.com>
2023-12-30 17:28:34 +0530

    Fully Optimized it for mobile screens. Also added a Footer section

--- script.js
+++
@@ -1,8 +0,0 @@
-let qr = document.getElementById(qr);
-
-function generateQR(){
-    if(qrText.value.length > 0){
-        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + qrText.value;
-        qr.classList.add("displayQR");
-    }   
-}
